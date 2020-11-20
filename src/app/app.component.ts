1fa0c469c9cd78f02725a35a40a5c3841958e129
ksrilal <ksrilalranasinghe95@gmail.com>
2020-11-20 11:14:03 +0530

    ksrilal_20201120

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import {MapInfoWindow, MapMarker} from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AGMD-app';
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
+  apiLoaded: Observable<boolean>;
+  name = "LAL";
+
+  constructor(httpClient: HttpClient) {
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyBVst5D1IiluQ1IlYfbo2xbAI4ogKRPx6A', 'callback')
+        .pipe(
+          map(() => true),
+          catchError(() => of(false)),
+        );
+  }
+
+  change(){
+    this.name = "Srilal";
+  }
+
+  center: google.maps.LatLngLiteral = {lat: 6.9271, lng: 79.8612};
+  zoom = 12;
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [{lat: 6.9118331, lng: 79.8513357}];
+
+  addMarker(event: google.maps.MouseEvent) {
+   // this.markerPositions.push(event.latLng.toJSON());
+    console.log(event.latLng.toJSON());
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    this.infoWindow.open(marker);
+    //console.log(this.route)
+  }
 }
